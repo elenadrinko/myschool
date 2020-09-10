0508debea9a8a251b5e4e80b935bdfd8e105bd3b
elenadrinko <56599050+elenadrinko@users.noreply.github.com>
2020-09-10 18:44:54 +0300

    Delete config.php

--- php/testing/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("HOST", "localhost");
-define("USER", "elen");
-define("PASS", "elen");
-define("DB", "school");
-
-$db = @mysqli_connect(HOST, USER, PASS, DB) or die('Нет соединения с БД');
-mysqli_set_charset($db, 'utf8') or die('Не установлена кодировка соединения');
